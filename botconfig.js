2b6a15f32d876c655ba967326fb07da0bce02eb9
PawitSahare <123424956+pawitpr@users.noreply.github.com>
2023-02-10 10:04:49 -0800

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -29,15 +29,7 @@ module.exports = {
 
   // You need a lavalink server for this bot to work!!!!
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
-  Lavalink: {
-    id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "https://pwit-bot.onrender.com", //- The host name or IP of the lavalink server.
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "9175861073", //- The password of the lavalink server.
-    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
-    retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
-    retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
-  },
+ 
   // Spotify Integration, allows you to enter a spotify link.
   Spotify: {
     ClientID: process.env.Spotify_ClientID || "", // Spotify Client ID
